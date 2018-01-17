6f9f2783d84533b0268a1b35dcc1499be3659a4a
Karl Ágústsson <kag@tskoli.is>
2018-01-17 10:01:40 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -3,10 +3,15 @@ from bottle import route, run
 
 @route('/')
 def index():
-  return "<a href='/about'>About</a>"
+  return "<a href='/about'>About</a>" /
+        "<a href='/contact'>Contact</a>"
 
 @route('/about')
 def about():
   return "<h2>About us</h2>"
+
+@route('/contact')
+def contact():
+  return "<h2>Contact us</h2>"
   
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
